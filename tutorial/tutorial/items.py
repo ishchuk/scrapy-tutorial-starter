8f2b1f1e7e4119fb532bc8d91da13bb5840e2e73
moancy <moancy@localhost.localdomain>
2020-04-23 14:01:45 +0200

    + items & .json example

--- tutorial/tutorial/items.py
+++ tutorial/tutorial/items.py
@@ -1,14 +1,10 @@
-# -*- coding: utf-8 -*-
+from scrapy.item import Item, Field
 
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
-import scrapy
-
-
-class TutorialItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class QuoteItem(Item):
+    quote_content = Field()
+    tags = Field()
+    author_name = Field()
+    author_birthday = Field()
+    author_bornlocation = Field()
+    author_bio = Field()
+    
