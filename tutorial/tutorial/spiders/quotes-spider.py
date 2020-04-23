8ccddbd5e5174234229c37d8cc9503132c3eb984
moancy <moancy@localhost.localdomain>
2020-04-23 10:53:37 +0200

    001 ыскфзук

---
+++ tutorial/tutorial/spiders/quotes-spider.py
@@ -0,0 +1,14 @@
+import scrapy
+
+class QuotesSpider(scrapy.Spider):
+    name = 'quotes'
+    start_urls = ['http://quotes.toscrape.com']
+    def parse(self,response):
+        self.logger.info('hello hello there')
+        quotes = response.css('div.quote')
+        for quote in quotes:
+            yield {
+                'text': quote.css('.text::text').get(),
+                'author': quote.css('.author::text').get(),
+                'tags': quote.css('.tag::text').getall(),
+            }
